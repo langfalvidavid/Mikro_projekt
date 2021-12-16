6e44bf85b6815133ea5ee1e2ec967b1e1d64eef5
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2021-12-16 18:39:21 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -1,6 +1,11 @@
 import RPi.GPIO as GPIO
 import time, datetime
+from pushbullet import Pushbullet
 
+GPIO.setwarnings(False)
+
+pb = Pushbullet("o.MA0CxmOB3mb4DL0Yp7WLHPpDSqp69fLM")
+print(pb.devices)
 Szenzor = 40
 Pumpa = 11
 
@@ -8,15 +13,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(Szenzor, GPIO.IN)
 GPIO.setup(Pumpa, GPIO.OUT)
 
-def Működés(Szenzor):
-  pontos_ido = datetime.datetime.now().strftime("%H:%M %Y-%m-%d")
-  if GPIO.input(Szenzor):
-    print("Száraz, vizet neki!")
-    GPIO.output(Pumpa, GPIO.LOW)
-  else:
-    print("Megöntözve ekkor: ", pontos_ido)
-    GPIO.output(Pumpa, GPIO.HIGH)
-  
-  while True:
-    Működés(Szenzor)
-    time.sleep(3)
+
+while True:
+    pontos_ido=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if GPIO.input(Szenzor):
+        GPIO.output(Pumpa, GPIO.LOW)
+        time.sleep(2)
+    else:
+        GPIO.output(Pumpa, GPIO.HIGH)
+        time.sleep(2)
+        dev=pb.get_device('Xiaomi Mi 9 SE')
+        push=dev.push_note("Megöntözve ekkor:", pontos_ido)
+    
